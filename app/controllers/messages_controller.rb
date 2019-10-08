a707b7d2e8f4434832433ff3e9b6a9c41251b919
Raul Alejandro Almanza Serrano <ra.almanzaserrano@gmail.com>
2019-10-08 11:38:01 -0500

    Add Edit and Delete Actions

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,7 @@
 class MessagesController < ApplicationController
 
+  before_action :find_message, only: [:show, :edit, :update, :destroy]
+
   def index
     @messages = Message.all.order("created_at DESC")
   end
@@ -20,14 +22,39 @@ class MessagesController < ApplicationController
   end
 
   def show
-    @message = Message.find(params[:id])
   end
 
+  def edit
+  end
+
+  def update
+      if @message.update(message_params)
+        flash[:success] = "Message was successfully updated"
+        redirect_to @message
+      else
+        flash[:error] = "Something went wrong"
+        render 'edit'
+      end
+  end
+
+  def destroy
+    if @message.destroy
+      flash[:success] = 'Message was successfully deleted.'
+      redirect_to root_path
+    else
+      flash[:error] = 'Something went wrong'
+      redirect_to @message
+    end
+  end
 
 private
 
   def message_params
     params.require(:message).permit(:title, :description)
+  end
+
+  def find_message
+    @message = Message.find(params[:id])
   end
 
 end
